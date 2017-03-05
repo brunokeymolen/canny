2792199417e71fa40c17daf84c386eba43c5a735
Bruno Keymolen <bruno.keymolen@gmail.com>
2017-03-05 11:50:38 +0100

    fixed local maxima

--- canny.cpp
+++ canny.cpp
@@ -6,26 +6,37 @@
 // Copyright (c) 2017, Bruno Keymolen, email: bruno.keymolen@gmail.com
 // All rights reserved.
 //
-// Redistribution and use in source and binary forms, with or without modification,
+// Redistribution and use in source and binary forms, with or without
+// modification,
 // are permitted provided that the following conditions are met:
 //
 // Redistributions of source code must retain the above copyright notice,
 // this list of conditions and the following disclaimer.
-// Redistributions in binary form must reproduce the above copyright notice, this
-// list of conditions and the following disclaimer in the documentation and/or other
+// Redistributions in binary form must reproduce the above copyright notice,
+// this
+// list of conditions and the following disclaimer in the documentation and/or
+// other
 // materials provided with the distribution.
 // Neither the name of "Bruno Keymolen" nor the names of its contributors may be
-// used to endorse or promote products derived from this software without specific
+// used to endorse or promote products derived from this software without
+// specific
 // prior written permission.
 //
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND
 // ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-// IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
-// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
-// NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED.
+// IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY
+// DIRECT,
+// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT
+// NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY,
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+// OTHERWISE)
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 //
@@ -35,14 +46,14 @@
 #include <cmath>
 #include <iostream>
 
-#include <cstdint>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
+#include <cstdint>
 #include <iomanip>
 
 namespace keymolen {
 
-
+// clang-format off
 
     //Sobel
     const int8_t Gx[] = {-1, 0, 1,
@@ -61,228 +72,204 @@ namespace keymolen {
     const int Gaus3x3Div = 16;
 
 
-	const int8_t Gaus5x5[] = { 	2,  4,  5,  4, 2,
-								4,  9, 12,  9, 4,
-								5, 12, 15, 12, 5, // * 1/159
-								4,  9, 12,  9, 4,
-								2,  4,  5,  4, 2 };
+    const int8_t Gaus5x5[] = { 	2,  4,  5,  4, 2,
+                                4,  9, 12,  9, 4,
+                                5, 12, 15, 12, 5, // * 1/159
+                                4,  9, 12,  9, 4,
+                                2,  4,  5,  4, 2 };
 
     const int Gaus5x5Div = 159;
 
+// clang-format on
 
+Canny::Canny(int w, int h) : w_(w), h_(h), size_(w * h) {
+    // clang-format off
+        /* 
+        //        blur                gradient/segment    local maxima    double threshold  hysterises  
+        //
+        //                                      /  G_  \
+        // src ->  dst  -> (Gx, Gy -> G_, t->s_)            G_(dst)    ->     d(dst)       ->  h(dst)    -> dst
+        // 
+        */
+    // clang-format on
 
-    Canny::Canny(int w, int h) :
-        w_(w), h_(h), size_(w*h)
-    {
+    G_ = (double*)calloc(w_ * h_, sizeof(double));
+    M_ = (double*)calloc(w_ * h_, sizeof(double));
+    s_ = (unsigned char*)calloc(w_ * h_, sizeof(unsigned char));
+}
 
-		/* 
-		//        blur                gradient/segment    local maxima    double threshold  hysterises  
-		//
-		//                                      /  G_  \
-		// src ->  dst  -> (Gx, Gy -> G_, t->s_)            G_(dst)    ->     d(dst)       ->  h(dst)    -> dst
-		// 
-		*/
-		 
-		 
-        G_ = (double*)calloc(w_ * h_, sizeof(double));
-		s_ =  (unsigned char*)calloc(w_ * h_, sizeof(unsigned char));
+Canny::~Canny() {
+    free(G_);
+    free(M_);
+    free(s_);
+}
+
+unsigned char* Canny::edges(unsigned char* dst, const unsigned char* src,
+                            Canny::NoiseFilter kernel_size, int weak_threshold,
+                            int strong_threshold) {
+    int offset_xy = 1;  // for kernel = 3
+    int8_t* kernel = (int8_t*)Gaus3x3;
+    int kernel_div = Gaus3x3Div;
+
+    if (kernel_size == NoiseFilter::Gaus5x5) {
+        offset_xy = 2;
+        kernel = (int8_t*)Gaus5x5;
+        kernel_div = Gaus5x5Div;
     }
 
-    Canny::~Canny()
-    {
-        free (G_);
-        free (s_);
-    }
+    // gaussian filter
 
-
-    unsigned char * Canny::edges(unsigned char *dst, const unsigned char* src, 
-			Canny::NoiseFilter kernel_size, int weak_threshold, int strong_threshold)
-    {
-
-        int offset_xy = 1; //for kernel = 3
-		int8_t* kernel = (int8_t*)Gaus3x3;
-		int kernel_div = Gaus3x3Div;
-
-		if (kernel_size == NoiseFilter::Gaus5x5)
-		{
-			offset_xy = 2;
-			kernel = (int8_t*)Gaus5x5;
-			kernel_div = Gaus5x5Div;
-		}
-
-        //gaussian filter 
-		
-        for (int x = 0; x < w_ ; x++) {
-            for (int y = 0; y < h_; y++) {
-                int pos = x + (y * w_);
-                if (x<offset_xy || x>=(w_-offset_xy) || y<offset_xy || y>=(h_-offset_xy)) {
-					dst[pos] = src[pos];
-					continue;
-				}
-				int convolve = 0;
-                int k = 0;
-                for (int kx = -offset_xy; kx <= offset_xy; kx++) {
-                    for (int ky = -offset_xy; ky <= offset_xy; ky++) {
-                        convolve += (src[pos + (kx + (ky * w_))] * kernel[k]);
-                        k++;
-                    }
+    for (int x = 0; x < w_; x++) {
+        for (int y = 0; y < h_; y++) {
+            int pos = x + (y * w_);
+            if (x < offset_xy || x >= (w_ - offset_xy) || y < offset_xy ||
+                y >= (h_ - offset_xy)) {
+                dst[pos] = src[pos];
+                continue;
+            }
+            int convolve = 0;
+            int k = 0;
+            for (int kx = -offset_xy; kx <= offset_xy; kx++) {
+                for (int ky = -offset_xy; ky <= offset_xy; ky++) {
+                    convolve += (src[pos + (kx + (ky * w_))] * kernel[k]);
+                    k++;
                 }
+            }
 
-                dst[pos] = (unsigned char)((double)convolve / (double)kernel_div);
+            dst[pos] = (unsigned char)((double)convolve / (double)kernel_div);
+        }
+    }
+
+    // apply sobel kernels
+    offset_xy = 1;  // 3x3
+    for (int x = offset_xy; x < w_ - offset_xy; x++) {
+        for (int y = offset_xy; y < h_ - offset_xy; y++) {
+            double convolve_X = 0.0;
+            double convolve_Y = 0.0;
+            int k = 0;
+            int src_pos = x + (y * w_);
+
+            for (int ky = -offset_xy; ky <= offset_xy; ky++) {
+                for (int kx = -offset_xy; kx <= offset_xy; kx++) {
+                    convolve_X += dst[src_pos + (kx + (ky * w_))] * Gx[k];
+                    convolve_Y += dst[src_pos + (kx + (ky * w_))] * Gy[k];
+
+                    k++;
+                }
+            }
+
+            // gradient hypot & direction
+            int segment = 0;
+
+            if (convolve_X == 0.0 || convolve_Y == 0.0) {
+                G_[src_pos] = 0;
+            } else {
+                G_[src_pos] = ((std::sqrt((convolve_X * convolve_X) +
+                                          (convolve_Y * convolve_Y))));
+                double theta = std::atan2(
+                    convolve_Y, convolve_X);  // radians. atan2 range: -PI,+PI,
+                                              // theta : 0 - 2PI
+                theta = theta * (360.0 / (2.0 * M_PI));  // degrees
+
+                if ((theta <= 22.5 && theta >= -22.5) || (theta <= -157.5) ||
+                    (theta >= 157.5)) {
+                    segment = 1;  // "-"
+                } else if ((theta > 22.5 && theta <= 67.5) ||
+                           (theta > -157.5 && theta <= -112.5)) {
+                    segment = 2;  // "/"
+                } else if ((theta > 67.5 && theta <= 112.5) ||
+                           (theta >= -112.5 && theta < -67.5)) {
+                    segment = 3;  // "|"
+                } else if ((theta >= -67.5 && theta < -22.5) ||
+                           (theta > 112.5 && theta < 157.5)) {
+                    segment = 4;  // "\"
+                } else {
+                    std::cout << "error " << theta << std::endl;
+                }
+            }
+
+            s_[src_pos] = (unsigned char)segment;
+
+            // std::cout << convolve_Y << "," << convolve_X << ":"<< theta << "
+            // seg:" << segment << " " << std::endl;
+        }
+    }
+
+    // local maxima: non maxima suppression
+    memcpy (M_, G_, w_ * h_ * sizeof(double));
+
+    for (int x = 1; x < w_ - 1; x++) {
+        for (int y = 1; y < h_ - 1; y++) {
+            int pos = x + (y * w_);
+
+            switch (s_[pos]) {
+                case 1:
+                    if (G_[pos - 1] >= G_[pos] || G_[pos + 1] >= G_[pos]) {
+                        M_[pos] = 0;
+                    }
+                    break;
+                case 2:
+                    if (G_[pos - (w_ - 1)] >= G_[pos] ||
+                        G_[pos + (w_ - 1)] >= G_[pos]) {
+                        M_[pos] = 0;
+                    }
+                    break;
+                case 3:
+                    if (G_[pos - (w_)] >= G_[pos] ||
+                        G_[pos + (w_)] >= G_[pos]) {
+                        M_[pos] = 0;
+                    }
+                    break;
+                case 4:
+                    if (G_[pos - (w_ + 1)] >= G_[pos] ||
+                        G_[pos + (w_ + 1)] >= G_[pos]) {
+                        M_[pos] = 0;
+                    }
+                    break;
+                default:
+                    M_[pos] = 0;
+                    break;
             }
         }
-
-        //apply sobel kernels 
-		offset_xy = 1; //3x3
-        for (int x = offset_xy; x < w_ - offset_xy; x++) {
-            for (int y = offset_xy; y < h_ - offset_xy; y++) {
-                double convolve_X = 0.0;
-                double convolve_Y = 0.0;
-                int k = 0;
-                int src_pos = x + (y * w_);
-
-                for (int ky = -offset_xy; ky <= offset_xy; ky++) {
-                	for (int kx = -offset_xy; kx <= offset_xy; kx++) {
-
-                        convolve_X += dst[src_pos + (kx + (ky * w_))] * Gx[k];
-                        convolve_Y += dst[src_pos + (kx + (ky * w_))] * Gy[k];
-						
-                        k++;
-                    }
-                }
-
-                //gradient hypot & direction
-				int segment = 0;
-
-				if (convolve_X == 0.0 || convolve_Y == 0.0)
-				{
-                	G_[src_pos] = 0;
-				}
-				else
-				{
-                	G_[src_pos] = ((std::sqrt((convolve_X*convolve_X) + (convolve_Y*convolve_Y))));
-					double theta = std::atan2(convolve_Y, convolve_X); //radians. atan2 range: -PI,+PI, theta : 0 - 2PI
-					theta = theta * (360.0 / (2.0*M_PI)); //degrees
-
-					if ((theta <= 22.5 && theta >= -22.5) || (theta <= -157.5) || (theta >= 157.5))
-					{
-						segment = 1; // "-"
-					} 
-					else if ((theta > 22.5 && theta <= 67.5) || (theta > -157.5 && theta <= -112.5)) 
-					{
-						segment = 2; // "/"
-					}
-					else if ((theta > 67.5 && theta <= 112.5) || (theta >= -112.5 && theta < -67.5)) 
-					{
-						segment = 3; // "|"
-					}
-					else if ((theta >= -67.5 && theta < -22.5) || (theta > 112.5 && theta < 157.5)) 
-					{
-						segment = 4; // "\"
-					}
-					else
-					{
-						std::cout << "error " << theta  << std::endl;
-					}
-				}
-
-				s_[src_pos] = (unsigned char)segment;
-
-				//std::cout << convolve_Y << "," << convolve_X << ":"<< theta << " seg:" << segment << " " << std::endl;	
-			}
-        }
-
-				
-		//local maxima: non maxima suppression 
-        for (int x = 1; x < w_ - 1; x++) {
-            for (int y = 1; y < h_ - 1; y++) {
-                int pos = x + (y * w_);
-
-				switch(s_[pos]) {
-				
-				case 1:
-					if ( G_[pos-1]>=G_[pos] || G_[pos+1]>=G_[pos])
-					{
-						G_[pos] = 0;
-					}
-					break;
-				case 2:
-					if ( G_[pos-(w_-1)]>=G_[pos] || G_[pos+(w_-1)]>=G_[pos])
-					{
-						G_[pos] = 0;
-					}
-					break;
-				case 3:
-					if (  G_[pos-(w_)]>=G_[pos] || G_[pos+(w_)]>=G_[pos])
-					{
-						G_[pos] = 0;
-					}break;
-				case 4:
-					if ( G_[pos-(w_+1)]>=G_[pos] || G_[pos+(w_+1)]>=G_[pos])
-					{
-						G_[pos] = 0;
-					}break;
-				default:
-					G_[pos] = 0;
-					break;
-				}
-			}
-		}
-
-		//double threshold
-        for (int x = 0; x < w_; x++) {
-            for (int y = 0; y < h_; y++) {
-                int src_pos = x + (y * w_);
-				if (G_[src_pos] > strong_threshold)
-				{
-					dst[src_pos] = 255;
-				}
-				else if(G_[src_pos] > weak_threshold)
-				{
-					dst[src_pos] = 100;
-				}
-				else
-				{
-					dst[src_pos] = 0;
-				}
-			}
-		}
-
-        //edges with hysteresis
-        for (int x = 1; x < w_ - 1; x++) {
-            for (int y = 1; y < h_ - 1; y++) {
-                int src_pos = x + (y * w_);
-				if (dst[src_pos] == 255)
-				{
-					dst[src_pos] = 255;
-				}
-				else if (dst[src_pos] == 100)
-				{
-					if (	dst[src_pos-1] == 255 ||
-							dst[src_pos+1] == 255 ||
-							dst[src_pos-1-w_] == 255 ||
-							dst[src_pos+1-w_] == 255 ||
-							dst[src_pos+w_] == 255 ||
-							dst[src_pos+w_-1] == 255 ||
-							dst[src_pos+w_+1] == 255)
-					{
-						dst[src_pos] = 255;
-					}
-					else
-					{
-						dst[src_pos] = 0;
-					}
-				}
-				else
-				{
-					dst[src_pos] = 0;
-				}
-
-			}
-		}
-
-		return dst;
     }
 
 
+    // double threshold
+    for (int x = 0; x < w_; x++) {
+        for (int y = 0; y < h_; y++) {
+            int src_pos = x + (y * w_);
+            if (M_[src_pos] > strong_threshold) {
+                dst[src_pos] = 255;
+            } else if (M_[src_pos] > weak_threshold) {
+                dst[src_pos] = 100;
+            } else {
+                dst[src_pos] = 0;
+            }
+        }
+    }
+
+    // edges with hysteresis
+    for (int x = 1; x < w_ - 1; x++) {
+        for (int y = 1; y < h_ - 1; y++) {
+            int src_pos = x + (y * w_);
+            if (dst[src_pos] == 255) {
+                dst[src_pos] = 255;
+            } else if (dst[src_pos] == 100) {
+                if (dst[src_pos - 1] == 255 || dst[src_pos + 1] == 255 ||
+                    dst[src_pos - 1 - w_] == 255 ||
+                    dst[src_pos + 1 - w_] == 255 || dst[src_pos + w_] == 255 ||
+                    dst[src_pos + w_ - 1] == 255 ||
+                    dst[src_pos + w_ + 1] == 255) {
+                    dst[src_pos] = 255;
+                } else {
+                    dst[src_pos] = 0;
+                }
+            } else {
+                dst[src_pos] = 0;
+            }
+        }
+    }
+
+    return dst;
+}
 }
--- main.cpp
+++ main.cpp
@@ -6,184 +6,188 @@
 // Copyright (c) 2017, Bruno Keymolen, email: bruno.keymolen@gmail.com
 // All rights reserved.
 //
-// Redistribution and use in source and binary forms, with or without modification,
+// Redistribution and use in source and binary forms, with or without
+// modification,
 // are permitted provided that the following conditions are met:
 //
 // Redistributions of source code must retain the above copyright notice,
 // this list of conditions and the following disclaimer.
-// Redistributions in binary form must reproduce the above copyright notice, this
-// list of conditions and the following disclaimer in the documentation and/or other
+// Redistributions in binary form must reproduce the above copyright notice,
+// this
+// list of conditions and the following disclaimer in the documentation and/or
+// other
 // materials provided with the distribution.
 // Neither the name of "Bruno Keymolen" nor the names of its contributors may be
-// used to endorse or promote products derived from this software without specific
+// used to endorse or promote products derived from this software without
+// specific
 // prior written permission.
 //
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND
 // ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-// IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
-// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
-// NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED.
+// IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY
+// DIRECT,
+// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT
+// NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY,
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+// OTHERWISE)
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 //
 // **********************************************************************************
-#include <stdio.h>
 #include <dirent.h>
+#include <stdio.h>
 #include <unistd.h>
-#include <string>
-#include <map>
 #include <iostream>
+#include <map>
+#include <string>
 
+#include "opencv2/core/core.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
-#include "opencv2/core/core.hpp"
 
 #include "canny.hpp"
 
-extern FILE *stdin;
-extern FILE *stdout;
-extern FILE *stderr;
+extern FILE* stdin;
+extern FILE* stdout;
+extern FILE* stderr;
 
 std::string img_path;
 int low_threshold = 30;
 int high_threshold = 100;
 
-const char* CW_IMG_ORIGINAL	= "Original";
-const char* CW_IMG_GRAY		= "Grayscale";
-const char* CW_IMG_EDGE		= "Canny Edge Detection";
+const char* CW_IMG_ORIGINAL = "Original";
+const char* CW_IMG_GRAY = "Grayscale";
+const char* CW_IMG_EDGE = "Canny Edge Detection";
 
 void doTransform(std::string);
 
-
-void usage(char * s)
-{
-
-	fprintf( stderr, "\n");
-    fprintf( stderr, "canny edge detection. build: %s-%s \n", __DATE__, __TIME__);
-    fprintf( stderr, "%s -s <source file> [-l <low threshold>] [-h <high threshold>] [-? help]", s);
-	fprintf( stderr, "   s: path image file\n");
-	fprintf( stderr, "   l: canny low threshold\n");
-	fprintf( stderr, "   h: canny high threshold\n");
-	fprintf( stderr, "\nexample:  ./canny -s ./img/russell-crowe-robin-hood-arrow.jpg -t 195\n");
-	fprintf( stderr, "\n");
+void usage(char* s) {
+    fprintf(stderr, "\n");
+    fprintf(stderr, "canny edge detection. build: %s-%s \n", __DATE__,
+            __TIME__);
+    fprintf(stderr,
+            "%s -s <source file> [-l <low threshold>] [-h <high threshold>] "
+            "[-? help]",
+            s);
+    fprintf(stderr, "   s: path image file\n");
+    fprintf(stderr, "   l: canny low threshold\n");
+    fprintf(stderr, "   h: canny high threshold\n");
+    fprintf(stderr,
+            "\nexample:  ./canny -s ./img/russell-crowe-robin-hood-arrow.jpg "
+            "-t 195\n");
+    fprintf(stderr, "\n");
 }
 
 int main(int argc, char** argv) {
+    int c;
+    while (((c = getopt(argc, argv, "s:l:h:?"))) != -1) {
+        switch (c) {
+            case 's':
+                img_path = optarg;
+                break;
+            case 'l':
+                low_threshold = atoi(optarg);
+                break;
+            case 'h':
+                high_threshold = atoi(optarg);
+                break;
+            case '?':
+            default:
+                usage(argv[0]);
+                return -1;
+        }
+    }
 
-	int c;
-	while ( ((c = getopt( argc, argv, "s:l:h:?" ) ) ) != -1 )
-	{
-	    switch (c)
-	    {
-	    case 's':
-	    	img_path = optarg;
-	    	break;
-	    case 'l':
-	    	low_threshold = atoi(optarg);
-	    	break;
-	    case 'h':
-	    	high_threshold = atoi(optarg);
-	    	break;
-		case '?':
-        default:
-			usage(argv[0]);
-			return -1;
-	    }
-	}
-
-	if(img_path.empty())
-	{
-		usage(argv[0]);
-		return -1;
-	}
+    if (img_path.empty()) {
+        usage(argv[0]);
+        return -1;
+    }
 
     cv::namedWindow(CW_IMG_ORIGINAL, cv::WINDOW_AUTOSIZE);
-    cv::namedWindow(CW_IMG_GRAY, 	 cv::WINDOW_AUTOSIZE);
-    cv::namedWindow(CW_IMG_EDGE, 	 cv::WINDOW_AUTOSIZE);
+    cv::namedWindow(CW_IMG_GRAY, cv::WINDOW_AUTOSIZE);
+    cv::namedWindow(CW_IMG_EDGE, cv::WINDOW_AUTOSIZE);
 
     cvMoveWindow(CW_IMG_ORIGINAL, 10, 10);
     cvMoveWindow(CW_IMG_GRAY, 680, 10);
     cvMoveWindow(CW_IMG_EDGE, 1350, 10);
 
     doTransform(img_path);
-	
-	double theta = M_PI + 2.01;
-	theta = theta * (360 / (2*M_PI)); //degrees
-	int segment = ((int)((theta + 22.5) / 45)) % 8 % 4  ;
-	
-	std::cout << theta << " : " << segment << std::endl;
 
+    double theta = M_PI + 2.01;
+    theta = theta * (360 / (2 * M_PI));  // degrees
+    int segment = ((int)((theta + 22.5) / 45)) % 8 % 4;
 
-	return 0;
+    std::cout << theta << " : " << segment << std::endl;
+
+    return 0;
 }
 
+void doTransform(std::string file_path) {
+    cv::Mat img_edge;
+    cv::Mat img_gray;
 
+    cv::Mat img_ori = cv::imread(file_path, 1);
+    cv::cvtColor(img_ori, img_gray, cv::COLOR_BGR2GRAY);
 
-void doTransform(std::string file_path)
-{
-	cv::Mat img_edge;
-	cv::Mat img_gray;
+    int w = img_gray.cols;
+    int h = img_ori.rows;
 
-	cv::Mat img_ori = cv::imread( file_path, 1 );
-	cv::cvtColor(img_ori, img_gray, cv::COLOR_BGR2GRAY);
+    keymolen::Canny::NoiseFilter filter = keymolen::Canny::NoiseFilter::Gaus3x3;
 
-	int w = img_gray.cols;
-	int h = img_ori.rows;
+    while (1) {
+        cv::Mat img_edge(h, w, CV_8UC1, cv::Scalar::all(0));
 
-	keymolen::Canny::NoiseFilter filter = keymolen::Canny::NoiseFilter::Gaus3x3;
+        keymolen::Canny canny(w, h);
+        canny.edges(img_edge.data, img_gray.data, filter, low_threshold,
+                    high_threshold);
 
-	while(1)
-	{
-		cv::Mat img_edge(h, w, CV_8UC1, cv::Scalar::all(0)); 
-	
-		keymolen::Canny canny(w, h);
-		canny.edges(img_edge.data, img_gray.data, filter, low_threshold, high_threshold);
+        // Visualize all
+        cv::imshow(CW_IMG_ORIGINAL, img_ori);
+        cv::imshow(CW_IMG_GRAY, img_gray);
+        cv::imshow(CW_IMG_EDGE, img_edge);
 
-		//Visualize all
-		cv::imshow(CW_IMG_ORIGINAL, img_ori);
-		cv::imshow(CW_IMG_GRAY, img_gray);
-		cv::imshow(CW_IMG_EDGE, img_edge);
+        char c = cv::waitKey(360000);
 
-		char c = cv::waitKey(360000);
-		
-		if(c == 'h') {
-			if (high_threshold > 10)
-				high_threshold -= 5;
-			else
-				high_threshold -= 1;
-		}
-		if(c == 'H')
-		{
-			if (high_threshold >= 10)
-				high_threshold += 5;
-			else
-				high_threshold += 1;
-		}
-		if(c == 'l') {
-			if (low_threshold > 10)
-				low_threshold -= 5;
-			else
-				low_threshold -= 1;
-		}
-		if(c == 'L') {
-			if (low_threshold >= 10)
-				low_threshold += 5;
-			else
-				low_threshold += 1;
-		}
-		if(c == '3')
-			filter = keymolen::Canny::NoiseFilter::Gaus3x3;
-		if(c == '5')
-			filter = keymolen::Canny::NoiseFilter::Gaus5x5;
-			
-		std::cout << low_threshold << ", " << high_threshold << std::endl;
+        if (c == 'h') {
+            if (high_threshold > 10)
+                high_threshold -= 5;
+            else
+                high_threshold -= 1;
+        }
+        if (c == 'H') {
+            if (high_threshold >= 10)
+                high_threshold += 5;
+            else
+                high_threshold += 1;
+        }
+        if (c == 'l') {
+            if (low_threshold > 10)
+                low_threshold -= 5;
+            else
+                low_threshold -= 1;
+        }
+        if (c == 'L') {
+            if (low_threshold >= 10)
+                low_threshold += 5;
+            else
+                low_threshold += 1;
+        }
+        if (c == '3') filter = keymolen::Canny::NoiseFilter::Gaus3x3;
+        if (c == '5') filter = keymolen::Canny::NoiseFilter::Gaus5x5;
+        if (c == 's') {
+            cv::imwrite("canny.png", img_edge);
+            std::cout << "write canny.png done..." << std::endl;
+        }
 
-		if(c == 27)
-			break;
-	}
+        std::cout << low_threshold << ", " << high_threshold << std::endl;
 
+        if (c == 27) break;
+    }
 }
-
